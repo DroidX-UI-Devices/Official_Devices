016b0e33609b2262768f02b78c22567cddeeec8e
Cykeek <Cykeek@proton.me>
2023-03-08 13:21:15 +0530

    V1.0 Changelogs for Realme 3 Pro

---
+++ RMX1851.mk
@@ -0,0 +1,14 @@
+==================
+Device Changelogs
+==================
+
+---------------------
+Date: March 08,2023
+---------------------
+
+- Initial Vanilla Build
+- Fixed some sepolices spams
+- Added more opimisation in kernel for better performance and battery
+- Added WPA3 in Hotspot section
+- Fixed some keyguard delays issues while unlocking device
+- Updated some blobs from C.18 rui1 a11
