64f0594bc4737e53ac37426f5b7775ba445e6058
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-21 21:56:57 +0700

    X01AD: Stable build DroidX-UI Official

--- X01AD.mk
+++ X01AD.mk
@@ -1,8 +1,18 @@
 ===================
 Device changelogs
 ===================
+Maintainer: Assunzain
+===================
 Date: August 20,2023
 ===================
 - Initial Build
 - using GLKernel with KSU
 - add support F2FS partition
+
+
+===================
+Date: August 21,2023
+===================
+- Stable build
+- Fixed Radio fm
+- Update GPS from LA.UM.9.6.3.r1-06200-89xx.0
